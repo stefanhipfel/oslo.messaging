3f62e6ce6cc816f9340badad494c08e56d55e380
Stefan Hipfel <stefan.hipfel@googlemail.com>
2022-04-04 15:59:10 +0200

    Update metrics.py

--- oslo_messaging/metrics.py
+++ oslo_messaging/metrics.py
@@ -1,25 +1,39 @@
 import logging
-import sys
-import functools
 import threading
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
 import statsd
 
 LOG = logging.getLogger(__name__)
 
-def threaded(fn):
-   def wrapper(*args, **kwargs):
-      thread = threading.Thread(target=fn, args=args, kwargs=kwargs)
-      thread.start()
-      return thread
-   return wrapper
-
-class Metrics():
+class Metrics(object):
    def __init__(self, host, port):
-      self._port = port
+      self.stop_event = threading.Event()
+      self.queue = queue.Queue()
       self.statsd = statsd.StatsClient(host, port)
 
-   @threaded
-   def counter(self, incoming, tag):
-      method = incoming.message.get('method')
-      stat = "{}.{}".format(tag, method)
-      self.statsd.incr(stat)
+   def start(self):
+      threading.Thread(target=self.counter, daemon=True).start()
+
+   def stop(self):
+      self.stop_event.set()
+
+   def counter(self):
+      while not self.stop_event.is_set():
+         try:
+            incoming = self.queue.get(timeout=1.0, block=True)
+         except queue.Empty:
+            continue
+
+         try:
+            message = incoming['message'].message
+            method = message.get('method')
+            stat = "oslo.messaging.{}.{}".format(method, incoming['tag'])
+            self.statsd.incr(stat)
+         except Exception:
+            LOG.exception('Exception during message metrics handling')
+         finally:
+            self.queue.task_done()
